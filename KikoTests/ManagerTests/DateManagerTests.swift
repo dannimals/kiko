b9af8d75121678af6b4b1168f4f2f66d62d7b2c6
Danning <danning.ge@gmail.com>
2018-08-06 18:54:16 -0400

    Fix dateManager bug

--- KikoTests/ManagerTests/DateManagerTests.swift
+++ KikoTests/ManagerTests/DateManagerTests.swift
@@ -4,35 +4,38 @@ import KikoModels
 @testable import Kiko
 
 class DateManagerTests: XCTestCase {
-    var dateManager: DateManageable?
-
-    override func setUp() {
-        super.setUp()
-
-        dateManager = DateManager(month: Month.february, year: 2018)
-    }
-
-    override func tearDown() {
-        super.tearDown()
-
-        dateManager = nil
-    }
+    lazy var augustDateManager: DateManageable = {
+        return DateManager(month: Month.august, year: 2018)
+    }()
+    lazy var febDateManager: DateManageable = {
+        return DateManager(month: Month.february, year: 2018)
+    }()
 
     func testNumberOfDays() {
-        XCTAssertEqual(dateManager!.numberOfDays, 28)
+        XCTAssertEqual(augustDateManager.numberOfDays, 31)
+        XCTAssertEqual(febDateManager.numberOfDays, 28)
     }
 
     func testMonth() {
-        XCTAssertEqual(dateManager!.month, Month.february)
+        XCTAssertEqual(augustDateManager.month, Month.august)
+        XCTAssertEqual(febDateManager.month, Month.february)
     }
 
     func testNumberOfDay() {
-        XCTAssertEqual(dateManager!.numberOf(day: .monday), 4)
-        XCTAssertEqual(dateManager!.numberOf(day: .tuesday), 4)
-        XCTAssertEqual(dateManager!.numberOf(day: .wednesday), 4)
-        XCTAssertEqual(dateManager!.numberOf(day: .thursday), 4)
-        XCTAssertEqual(dateManager!.numberOf(day: .friday), 4)
-        XCTAssertEqual(dateManager!.numberOf(day: .saturday), 4)
-        XCTAssertEqual(dateManager!.numberOf(day: .sunday), 4)
+        XCTAssertEqual(augustDateManager.numberOf(day: .monday), 4)
+        XCTAssertEqual(augustDateManager.numberOf(day: .tuesday), 4)
+        XCTAssertEqual(augustDateManager.numberOf(day: .wednesday), 5)
+        XCTAssertEqual(augustDateManager.numberOf(day: .thursday), 5)
+        XCTAssertEqual(augustDateManager.numberOf(day: .friday), 5)
+        XCTAssertEqual(augustDateManager.numberOf(day: .saturday), 4)
+        XCTAssertEqual(augustDateManager.numberOf(day: .sunday), 4)
+
+        XCTAssertEqual(febDateManager.numberOf(day: .monday), 4)
+        XCTAssertEqual(febDateManager.numberOf(day: .tuesday), 4)
+        XCTAssertEqual(febDateManager.numberOf(day: .wednesday), 4)
+        XCTAssertEqual(febDateManager.numberOf(day: .thursday), 4)
+        XCTAssertEqual(febDateManager.numberOf(day: .friday), 4)
+        XCTAssertEqual(febDateManager.numberOf(day: .saturday), 4)
+        XCTAssertEqual(febDateManager.numberOf(day: .sunday), 4)
     }
 }
