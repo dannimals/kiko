9488e3171e86522a70e12f8bcbb38b45f04e8642
Danning <danning.ge@gmail.com>
2018-08-19 15:44:03 -0400

    Begin mood navigation coordinator

--- Kiko/AppDelegate.swift
+++ Kiko/AppDelegate.swift
@@ -4,17 +4,17 @@ import KikoUIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    lazy var appCoordinator: AppCoordinating? = {
+    lazy var mainCoordinator: AppCoordinating? = {
         guard let moodManager = try? MoodManager() else { return nil }
         let calendarManager = CalendarManager(date: Date())
-        return AppCoordinator(window: window, calendarManager: calendarManager, moodManager: moodManager)
+        return MainNavigationCoordinator(window: window, calendarManager: calendarManager, moodManager: moodManager)
     }()
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         window = UIWindow(frame: UIScreen.main.bounds)
-        appCoordinator?.start()
+        mainCoordinator?.start()
         return true
     }
 
