541dec5778084336295b3add50acc412957ac3e5
Danning <danning.ge@gmail.com>
2018-08-21 21:51:25 -0400

    Show completion staet

--- Kiko/Protocols/UIViewControllerAlertable.swift
+++ Kiko/Protocols/UIViewControllerAlertable.swift
@@ -2,10 +2,11 @@ import KikoUIKit
 
 protocol UIViewControllerAlertable where Self: UIViewController {
     func presentModalForSuccess(withImage image: UIImage, imageColor: UIColor)
-    func presentModalForFailure(withError error: Error, message: String)
+    func presentModalForFailure(withError error: Error?, message: String)
 }
 
 extension UIViewController: UIViewControllerAlertable {
+
     func presentModalForSuccess(withImage image: UIImage = #imageLiteral(resourceName: "splatter"), imageColor: UIColor) {
         let completionStateViewController: MoodLogCompletionStateViewController = MoodLogCompletionStateViewController.loadFromNib()
         completionStateViewController.configure(withImage: image, imageColor: imageColor)
@@ -13,13 +14,19 @@ extension UIViewController: UIViewControllerAlertable {
         present(completionStateViewController, animated: true, completion: { () -> Void in
             let delayTime = DispatchTime.now() + 1
             DispatchQueue.main.asyncAfter(deadline: delayTime) {
-                self.dismiss(animated: true, completion: nil)
+                completionStateViewController.prepareForDismissal()
             }
         })
     }
 
-    func presentModalForFailure(withError error: Error, message: String) {
-
+    func presentModalForFailure(withError error: Error?, message: String) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        present(alertController, animated: true, completion: { () -> Void in
+            let delayTime = DispatchTime.now() + 1
+            DispatchQueue.main.asyncAfter(deadline: delayTime) {
+                self.dismiss(animated: true, completion: nil)
+            }
+        })
     }
 
 }
