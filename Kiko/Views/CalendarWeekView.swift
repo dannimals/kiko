b549223002b0a69848d574e1767696fc96f9ec1f
Danning <danning.ge@gmail.com>
2018-09-11 22:32:06 -0400

    WTF scroll view

--- Kiko/Views/CalendarWeekView.swift
+++ Kiko/Views/CalendarWeekView.swift
@@ -1,5 +1,10 @@
 import KikoUIKit
 
+enum ScrollDirection: Int {
+    case left = -1
+    case right = 1
+}
+
 class CalendarWeekView: UIView {
 
     @IBOutlet weak var monthLabel: UILabel!
@@ -7,9 +12,13 @@ class CalendarWeekView: UIView {
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var datesCollectionView: WeekCalendarCollectionView!
 
+    var scrollDirection: ScrollDirection = .right
     var userDidScroll = false
     let leftButtonTapped = Channel<UIControlEvents>()
     let rightButtonTapped = Channel<UIControlEvents>()
+
+    private var contentOffsetX: CGFloat = 0
+    private var beginOffsetX: CGFloat = 0
 
     @objc private func notifyLeftButtonTappedEvent() { leftButtonTapped.broadcast(UIControlEvents.touchUpInside) }
     @objc private func notifyRightButtonTappedEvent() { rightButtonTapped.broadcast(UIControlEvents.touchUpInside) }
@@ -19,7 +28,7 @@ class CalendarWeekView: UIView {
         datesCollectionView.delegate = self
         datesCollectionView.registerCell(CalendarDayCollectionViewCell.self)
         datesCollectionView.configure(delegate: dataSource)
-
+        datesCollectionView.decelerationRate = UIScrollViewDecelerationRateFast
         setupEvents()
     }
 
@@ -50,10 +59,42 @@ class CalendarWeekView: UIView {
 
 extension CalendarWeekView: UICollectionViewDelegate {
 
+    var screenWidth: CGFloat {
+        return bounds.width
+    }
+
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        userDidScroll = true
+        beginOffsetX =  scrollView.contentOffset.x / screenWidth
+    }
+
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        guard userDidScroll else { return }
+        guard userDidScroll && scrollView.panGestureRecognizer.isEnabled else { return }
+        contentOffsetX = scrollView.contentOffset.x
+        scrollDirection = contentOffsetX > beginOffsetX ? .right : .left
+//        if fabs(contentOffsetX) - fabs(beginOffsetX) >= 0.3 * screenWidth {
+//            print(Int(beginOffsetX / screenWidth))
+//            let count =  Int(beginOffsetX / screenWidth) + scrollDirection.rawValue
+//            let maxScrollOffset =  screenWidth * CGFloat(count)
+//            capScrollView(scrollView, withOffset: maxScrollOffset, direction: scrollDirection)
+//        }
+
+        //        datesCollectionView.recenterIfNecessary()
+    }
+
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        capScrollView(scrollView, withOffset: contentOffsetX, direction: scrollDirection)
+    }
+
+    private func capScrollView(_ scrollView: UIScrollView, withOffset offset: CGFloat, direction: ScrollDirection) {
         userDidScroll = false
-        datesCollectionView.recenterIfNecessary()
+        let scrollToOffset = cappedCurrentOffset(offset, direction: direction)
+        UIView.animate(withDuration: 0.2) { scrollView.contentOffset.x = scrollToOffset }
+    }
+
+    private func cappedCurrentOffset(_ currentOffset: CGFloat, direction: ScrollDirection) -> CGFloat {
+        let roundWidths = round(currentOffset / screenWidth)
+        return roundWidths * screenWidth
     }
 
 }
