0f6136358474ea686b50eef24161a78402eaa007
Danning <danning.ge@gmail.com>
2018-09-17 23:15:13 -0400

    More cleanup:

--- Kiko/Views/Cells/CalendarDayCollectionViewCell.swift
+++ Kiko/Views/Cells/CalendarDayCollectionViewCell.swift
@@ -1,3 +1,4 @@
+import KikoModels
 import KikoUIKit
 
 class CalendarDayCollectionViewCell: UICollectionViewCell {
@@ -5,7 +6,7 @@ class CalendarDayCollectionViewCell: UICollectionViewCell {
     var isDaySelected = false
     let dateLabel = UILabel()
     private let backgroundCircleView = UIView()
-    private let indicatorCircle = UIView()
+    private let moodIndicatorCircle = UIView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -13,26 +14,32 @@ class CalendarDayCollectionViewCell: UICollectionViewCell {
         configure()
     }
 
-    func configure(day: Int, shouldShowBackgroundCircle: Bool, font: UIFont, textColor: UIColor, moodColor: UIColor?) {
-        dateLabel.text = day.description
-        backgroundCircleView.isHidden = !shouldShowBackgroundCircle
-        configureDayLabel(font: font, textColor: textColor)
-        if let moodColor = moodColor {
-            configureBackgroundCircleView(isHidden: !shouldShowBackgroundCircle, moodColor: moodColor)
+    func configure(date: Date, today: Date, moodColor: UIColor?) {
+        let isToday = date ≈≈ today
+        configureCircleViews(isToday: isToday, moodColor: moodColor)
+        if isToday {
+            configureDateLabel(font: .customFont(ofSize: 16, weight: .heavy), textColor: .salmonPink)
+        } else if date < today {
+            configureDateLabel(font: .customFont(ofSize: 14, weight: .medium), textColor: .textDarkGrey)
+        } else {
+            configureDateLabel(font: .customFont(ofSize: 14, weight: .light), textColor: .textLightGrey)
         }
+        dateLabel.text = date.day.description
     }
 
-    private func configureBackgroundCircleView(isHidden: Bool, moodColor: UIColor) {
-        guard isHidden else {
+    private func configureCircleViews(isToday: Bool, moodColor: UIColor?) {
+        backgroundCircleView.isHidden = !isToday
+        guard let moodColor = moodColor else { return }
+        if isToday {
             backgroundCircleView.layer.borderWidth = 2
             backgroundCircleView.layer.borderColor = moodColor.cgColor
-            return
+        } else {
+            moodIndicatorCircle.isHidden = false
+            moodIndicatorCircle.backgroundColor = moodColor
         }
-        indicatorCircle.backgroundColor = moodColor
-        indicatorCircle.isHidden = false
     }
 
-    private func configureDayLabel(font: UIFont, textColor: UIColor) {
+    private func configureDateLabel(font: UIFont, textColor: UIColor) {
         dateLabel.font = font
         dateLabel.textColor = textColor
     }
@@ -48,7 +55,7 @@ class CalendarDayCollectionViewCell: UICollectionViewCell {
     private func configure() {
         configureForReuse()
         configureBackgroundCircle()
-        configureIndicatorCircle()
+        configureMoodIndicatorCircle()
         configureDateLabel()
     }
 
@@ -77,20 +84,20 @@ class CalendarDayCollectionViewCell: UICollectionViewCell {
         ])
     }
 
-    private func configureIndicatorCircle() {
-        indicatorCircle.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(indicatorCircle)
-        indicatorCircle.layer.cornerRadius = 3.5
+    private func configureMoodIndicatorCircle() {
+        moodIndicatorCircle.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(moodIndicatorCircle)
+        moodIndicatorCircle.layer.cornerRadius = 3.5
         NSLayoutConstraint.activate([
-            indicatorCircle.heightAnchor.constraint(equalToConstant: 7),
-            indicatorCircle.widthAnchor.constraint(equalToConstant: 7),
-            indicatorCircle.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            indicatorCircle.topAnchor.constraint(equalTo: contentView.topAnchor)
+            moodIndicatorCircle.heightAnchor.constraint(equalToConstant: 7),
+            moodIndicatorCircle.widthAnchor.constraint(equalToConstant: 7),
+            moodIndicatorCircle.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            moodIndicatorCircle.topAnchor.constraint(equalTo: contentView.topAnchor)
         ])
     }
 
     private func configureForReuse() {
-        indicatorCircle.isHidden = true
+        moodIndicatorCircle.isHidden = true
         backgroundCircleView.isHidden = true
         dateLabel.font = UIFont.customFont(ofSize: 14, weight: .medium)
         dateLabel.textColor = UIColor.textDarkGrey
