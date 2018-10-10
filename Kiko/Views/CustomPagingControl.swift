c93af7f4ec57d051ce3cd4de22e529a0155d346a
Danning <danning.ge@gmail.com>
2018-10-09 22:17:46 -0400

    fix mood scroll

--- Kiko/Views/CustomPagingControl.swift
+++ Kiko/Views/CustomPagingControl.swift
@@ -3,10 +3,23 @@ import KikoUIKit
 
 class CustomPagingControl: UIView {
 
-    private let defaultColor = UIColor.cornflowerYellow
+    private let defaultSelectedColor = UIColor.cornflowerYellow
     private let circleStackView = UIStackView()
+    private(set) var selectedIndex: Int = 0
+
+    init() {
+        super.init(frame: .zero)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        setup()
+    }
 
     func update(selectedIndex: Int, color: UIColor) {
+        self.selectedIndex = selectedIndex
         circleStackView.arrangedSubviews.forEach { $0.backgroundColor = color.faded }
         circleStackView.arrangedSubviews[selectedIndex].backgroundColor = color
     }
@@ -20,13 +33,13 @@ class CustomPagingControl: UIView {
         return circleView
     }
 
-    func setup() {
+    private func setup() {
         for _ in 0..<4 { circleStackView.addArrangedSubview(addCircleView()) }
         circleStackView.axis = .horizontal
         circleStackView.distribution = .equalCentering
         circleStackView.spacing = 7
-        circleStackView.arrangedSubviews.forEach { $0.backgroundColor = defaultColor.faded }
-        circleStackView.arrangedSubviews.first?.backgroundColor = defaultColor
+        circleStackView.arrangedSubviews.forEach { $0.backgroundColor = defaultSelectedColor.faded }
+        circleStackView.arrangedSubviews.first?.backgroundColor = defaultSelectedColor
         circleStackView.stretchToFill(parentView: self)
     }
 
