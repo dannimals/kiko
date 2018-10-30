8da857616377b7468dd89d0b582fb53563f0b456
Danning <danning.ge@gmail.com>
2018-10-29 22:07:06 -0400

    Begin local notifications

---
+++ Kiko/Notifications/NotificationCoodinator.swift
@@ -0,0 +1,71 @@
+
+import UserNotifications
+import KikoModels
+
+protocol NotificationCoordinating: class {
+    func start()
+    func add(notification: Notification, trigger: NotificationTrigger)
+}
+
+protocol NotificationHandling: class {
+    func add(_ request: UNNotificationRequest, withCompletionHandler completionHandler: ((Error?) -> Void)?)
+    func getNotificationSettings(completionHandler: @escaping (UNNotificationSettings) -> Void)
+    func requestAuthorization(options: UNAuthorizationOptions, completionHandler: @escaping (Bool, Error?) -> Void)
+}
+
+struct NotificationTrigger {
+
+    var trigger: UNTimeIntervalNotificationTrigger
+
+    init(timeInterval: TimeInterval, repeats: Bool) {
+        trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: repeats)
+    }
+}
+
+extension UNUserNotificationCenter: NotificationHandling {}
+
+struct Notification {
+
+    let content = UNMutableNotificationContent()
+
+    init(title: String, subtitle: String, body: String) {
+        content.title = title
+        content.subtitle = subtitle
+        content.body = body
+    }
+}
+
+class NotificationCoordinator: NotificationCoordinating {
+
+    private let notificationHandler: NotificationHandling
+    private static let notificationID = "com.Kiko.notificationID"
+
+    init(notificationHandler: NotificationHandling) {
+        self.notificationHandler = notificationHandler
+    }
+
+    func add(notification: Notification, trigger: NotificationTrigger) {
+        let request = UNNotificationRequest(identifier: NotificationCoordinator.notificationID, content: notification.content, trigger: trigger.trigger)
+        notificationHandler.add(request, withCompletionHandler: nil)
+    }
+
+    func start() {
+        notificationHandler.getNotificationSettings { [weak self] settings in
+            guard settings.authorizationStatus == .authorized else {
+                self?.requestAuthorization()
+                return
+            }
+        }
+    }
+
+    private func requestAuthorization() {
+        notificationHandler.requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] didAllow, _ in
+            if !didAllow { self?.handleRejectedAuthorization() }
+        }
+    }
+
+    private func handleRejectedAuthorization() {
+
+    }
+
+}
