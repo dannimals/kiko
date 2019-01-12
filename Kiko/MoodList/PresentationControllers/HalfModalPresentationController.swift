5e6ee01fb5810a0e3ac2bc3c72c1398e376e9b1f
Danning <danning.ge@gmail.com>
2019-01-11 20:38:45 -0500

    Hide show arrow based on touch point

--- Kiko/MoodList/PresentationControllers/HalfModalPresentationController.swift
+++ Kiko/MoodList/PresentationControllers/HalfModalPresentationController.swift
@@ -62,17 +62,20 @@ class HalfModalPresentationController: UIPresentationController {
             switch state {
             case .halfScreen:
                 presentedView.frame.origin.y = touchPoint.y + containerView.frame.height / 2
-                if let moodListView = presentedView as? MoodListView {
-                    let previousOffset = moodListView.downwardArrow.centerYOffset
-                    moodListView.downwardArrow.centerYOffset = velocity.y < 0 ? previousOffset - 0.1 : previousOffset + 0.1
-                }
             case .fullScreen:
                 presentedView.frame.origin.y = touchPoint.y
+            }
+            if let moodListView = presentedView as? MoodListView {
+                let previousOffset = moodListView.downwardArrow.centerYOffset
+                moodListView.downwardArrow.centerYOffset = velocity.y < 0 ? previousOffset - 0.1 : previousOffset + 0.1
             }
             direction = velocity.y
         case .ended:
             if direction < 0 {
                 updateModalForState(.fullScreen)
+                if let moodListView = presentedView as? MoodListView {
+                    moodListView.downwardArrow.centerYOffset = 0
+                }
             } else {
                 presentedViewController.dismiss(animated: true, completion: nil)
             }
--- Kiko/Views/AnimatedDownwardArrow.swift
+++ Kiko/Views/AnimatedDownwardArrow.swift
@@ -6,6 +6,7 @@ class AnimatedDownwardArrow: UIView {
     private let strokeColor = UIColor.white
     private let lineWidth: CGFloat = 4
     private let path = UIBezierPath()
+    private let startPoint = CGPoint(x: 4, y: 4)
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,17 +27,57 @@ class AnimatedDownwardArrow: UIView {
     override func draw(_ rect: CGRect) {
         super.draw(rect)
 
-        drawArrow(centerYOffset: centerYOffset)
+        update(centerYOffset: centerYOffset)
     }
 
-    func drawArrow(centerYOffset: CGFloat = 0) {
+    private func update(centerYOffset: CGFloat = 0) {
+        let minMidpointOffsetY = bounds.height / 2
+        guard centerYOffset >= 0 else {
+            alpha = 1
+            drawArrow(midPoint: bounds.height - lineWidth)
+            return
+        }
+        let midPointOffsetY = bounds.height - centerYOffset - lineWidth
+        guard midPointOffsetY > minMidpointOffsetY else {
+            drawArrow(midPoint: midPointOffsetY)
+            hide()
+            return
+        }
+        show()
+        drawArrow(midPoint: midPointOffsetY)
+    }
+
+    private func drawArrow(midPoint: CGFloat) {
         path.removeAllPoints()
-        path.move(to: CGPoint(x: 4, y: 4))
-        path.addLine(to: CGPoint(x: bounds.width / 2, y: bounds.height - centerYOffset - lineWidth))
+        path.move(to: startPoint)
+        path.addLine(to: CGPoint(x: bounds.width / 2, y: midPoint))
         path.addLine(to: CGPoint(x: bounds.width - lineWidth, y: lineWidth))
         path.lineWidth = lineWidth
         strokeColor.set()
         path.lineCapStyle = .round
         path.stroke()
+    }
+
+    private func drawArrorWithoutOffset() {
+        path.removeAllPoints()
+        path.move(to: startPoint)
+        path.addLine(to: CGPoint(x: bounds.width / 2, y: bounds.height - lineWidth))
+        path.addLine(to: CGPoint(x: bounds.width - lineWidth, y: lineWidth))
+        path.lineWidth = lineWidth
+        strokeColor.set()
+        path.lineCapStyle = .round
+        path.stroke()
+    }
+
+    private func hide() {
+        UIView.animate(withDuration: 0.4) {
+            self.alpha = 0
+        }
+    }
+
+    private func show() {
+        UIView.animate(withDuration: 0.4) {
+            self.alpha = 1
+        }
     }
 }
